8ee316cafc01a7637d29e3f86d0b598ee7388a15
Drew Cappel <drewcappel@drews-macbook-pro.local>
2019-02-10 19:35:38 -0500

    4.3 Complete

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageToPunch: UIImageView!
-    
+    var audioPlayer = AVAudioPlayer()
     
     
     
@@ -29,6 +30,21 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
     }
     
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        // playing sounds
+        // attempt to load in sound name
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("Error: \(soundName) not found.")
+            }
+        } else {
+            // if load-in did not work
+            print("Error: \(soundName) not found.")
+        }
+    }
     
     // MARK: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
@@ -39,7 +55,7 @@ class ViewController: UIViewController {
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
-    
 }
 
